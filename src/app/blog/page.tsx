3cd8331e0413588d86417acb83b392970ccc97cc
Cordovana Enzo <bnk4970@gmail.com>
2024-09-26 14:33:10 +0200

    add svg

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -6,7 +6,7 @@ export default async function BlogsPage() {
   return (
     <main className="flex flex-col gap-4">
       {blogs.map((blog, i) => (
-        <article key={i} className="grid grid-rows-4 text-3xl p-2 outline outline-2 outline-[rgb(var(--secondary))] rounded-md">
+        <article key={i} className="grid grid-rows-4 text-3xl p-2 border-2 border-[rgb(var(--secondary))] rounded-md">
           <h1>{blog.frontmatter.title}</h1>
           <p>{blog.frontmatter.author}</p>
           <p>{blog.frontmatter.publishDate}</p>
--- src/app/course/page.tsx
+++ src/app/course/page.tsx
@@ -1,21 +1,44 @@
 import Button from "@/components/CTA/Button";
+import DB from "../../assets/svg/database.svg";
+import REACT from "../../assets/svg/react.svg";
+import NEXT from "../../assets/svg/nextdotjs.svg";
+import GIT from "../../assets/svg/git.svg";
+import BASH from "../../assets/svg/gnubash.svg";
+import BLEND from "../../assets/svg/blend.svg";
+import Image from "next/image";
 interface CardProps {
   label: string;
   description: string;
   prix: number;
+  logo: string;
 }
 export const Card: React.FC<CardProps> = (props) => {
   return (
-    <div className="grid grid-rows-[1fr_auto] gap-4 outline outline-2 outline-[rgb(var(--secondary))] rounded-md p-4">
-      <div className="flex flex-col">
-        <h3 className="uppercase text-xl">{props.label}</h3>
-        <p className="text-[#aaa]">{props.description}</p>
+    <div className="grid grid-rows-[1fr_auto] gap-4 outline outline-2 outline-[rgb(var(--secondary))] rounded-md p-4 relative overflow-clip">
+      <div className="flex flex-col row-span-1 gap-1">
+        <div className="flex items-center justify-between gap-2">
+          <h3 className="uppercase text-xl">{props.label}</h3>
+          <span className="font-semibold text-xl text-[rgb(var(--primary))]">{props.prix}€</span>
+        </div>
+        <p className="text-[rgb(var--foreground)]">{props.description}</p>
       </div>
-      <div className="flex items-center justify-between w-full gap-2">
-        <span className="font-semibold text-xl">{props.prix}€</span>
+      <div className="flex items-center justify-start w-full gap-4 row-span-1">
+        {/* <Button variant="solid" color="secondary" radius="md">
+          Afficher plus
+        </Button> */}
         <Button variant="solid" color="primary" radius="md">
           Acheter
         </Button>
+      </div>
+      <div className="absolute -z-10 top-0 w-full h-full grid place-items-center bg-[rgb(var(--secondary),0.3)]">
+        <Image
+          priority
+          src={props.logo}
+          height="200"
+          width="200"
+          alt=""
+          className="ml-auto -rotate-12 opacity-30"
+        />
       </div>
     </div>
   );
@@ -25,33 +48,38 @@ export default function Home() {
   const COURSES = [
     {
       label: "bash",
-      description: " Apprenez à automatiser vos tâches et à optimiser votre productivité en maîtrisant les bases et les commandes avancées du terminal Bash.",
+      description:
+        " Apprenez à automatiser vos tâches et à optimiser votre productivité en maîtrisant les bases et les commandes avancées du terminal Bash.",
       prix: 39,
-      logo: "logo"
+      logo: BASH,
     },
     {
       label: "git & github",
-      description: "Comprenez Git en profondeur, de l’initiation au contrôle de version, jusqu'à la gestion de projets en équipe avec GitHub.",
+      description:
+        "Comprenez Git en profondeur, de l’initiation au contrôle de version, jusqu'à la gestion de projets en équipe avec GitHub.",
       prix: 49,
-      logo: "logo",
+      logo: GIT,
     },
     {
       label: "front-end",
-      description: "Devenez un expert en création d’interfaces web modernes et responsive grâce à ce cours complet.",
+      description:
+        "Devenez un expert en création d’interfaces web modernes et responsive grâce à ce cours complet.",
       prix: 59,
-      logo: "logo"
+      logo: BLEND,
     },
     {
       label: "back-end",
-      description: "Apprenez à construire des applications robustes côté serveur avec Node.js, et à gérer les bases de données NoSQL avec MongoDB.",
+      description:
+        "Apprenez à construire des applications robustes côté serveur avec Node.js, et à gérer les bases de données NoSQL avec MongoDB.",
       prix: 69,
-      logo: "logo"
+      logo: DB,
     },
     {
       label: "react & next js",
-      description: "Plongez dans la création de sites et d’applications web ultra performantes avec React et Next.js, deux des frameworks JavaScript les plus populaires.",
+      description:
+        "Plongez dans la création de sites et d’applications web ultra performantes avec React et Next.js, deux des frameworks JavaScript les plus populaires.",
       prix: 79,
-      logo: "logo",
+      logo: REACT,
     },
   ];
   return (
@@ -69,7 +97,13 @@ export default function Home() {
         <h2 className="uppercase text-3xl">Des cours au choix à disposition</h2>
         <div className=" grid grid-cols-1 gap-4 py-4">
           {COURSES.map((card, idx) => (
-            <Card key={idx} label={card.label} description={card.description} prix={card.prix}/>
+            <Card
+              key={idx}
+              label={card.label}
+              description={card.description}
+              prix={card.prix}
+              logo={card.logo}
+            />
           ))}
         </div>
       </section>
